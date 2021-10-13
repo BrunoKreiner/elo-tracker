290a1b107da809639b9b30da5f04cd7f38748c3e
Alex Chao <ac590@duke.edu>
2021-10-12 23:10:40 -0400

    new homepage is connected to database

--- app/home.py
+++ app/home.py
@@ -3,6 +3,10 @@ from flask_login import current_user
 import datetime
 
 
+from .models.product import Product
+from .models.purchase import Purchase
+
+
 
 from flask import Blueprint
 bp = Blueprint('home', __name__)
@@ -10,5 +14,16 @@ bp = Blueprint('home', __name__)
 
 @bp.route('/home')
 def home():
+    # get all available products for sale:
+    products = Product.get_all(True)
+    # find the products current user has bought:
+    if current_user.is_authenticated:
+        purchases = Purchase.get_all_by_uid_since(
+            current_user.id, datetime.datetime(1980, 9, 14, 0, 0, 0))
+    else:
+        purchases = None
+    # render the page by adding information to the index.html file
+    return render_template('homepage.html',
+                           avail_products=products,
+                           purchase_history=purchases)
 
-    return render_template('homepage.html')
