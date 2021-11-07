c4d17d1bc6a857ed6d6939cdb3664631e9be0db9
Katherine Barbano <kab134@duke.edu>
2021-11-07 17:24:15 -0500

    pull elo average stats

--- app/home.py
+++ app/home.py
@@ -2,9 +2,7 @@ from flask import render_template
 from flask_login import current_user
 import datetime
 
-
-from .models.product import Product
-from .models.purchase import Purchase
+from .models.elo import elo_ref
 
 
 
@@ -15,15 +13,8 @@ bp = Blueprint('home', __name__)
 @bp.route('/home')
 def home():
     # get all available products for sale:
-    products = Product.get_all(True)
-    # find the products current user has bought:
-    if current_user.is_authenticated:
-        purchases = Purchase.get_all_by_uid_since(
-            current_user.rankable_id, datetime.datetime(1980, 9, 14, 0, 0, 0))
-    else:
-        purchases = None
+    averageElo = elo_ref.get_average(current_user.rankable_id)
     # render the page by adding information to the index.html file
     return render_template('homepage.html',
-                           avail_products=products,
-                           purchase_history=purchases)
+                           averageElo=averageElo)
 
