d816273c1be63340ce09d4635e10ecc0c5af797c
Katherine Barbano <kab134@duke.edu>
2021-11-08 17:54:55 -0500

    dropdown for category

--- app/rankables.py
+++ app/rankables.py
@@ -2,7 +2,7 @@ from flask import render_template, redirect, url_for, flash, request
 from werkzeug.urls import url_parse
 from flask_login import login_user, logout_user, current_user
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField
 from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
 from flask_babel import _, lazy_gettext as _l
 
@@ -48,7 +48,7 @@ class RegistrationForm(FlaskForm):
         _l('Repeat Password'), validators=[DataRequired(),
                                            EqualTo('password')])
     about = StringField(_l('About'), validators=[DataRequired()])
-    category = StringField(_l('Category'), validators=[DataRequired()])
+    category = SelectField('Category', choices=[('People', 'People'), ('Restaurant', 'Restaurant'), ('Code Editor', 'Code Editor'), ('School','School')])
     submit = SubmitField(_l('Register'))
 
     def validate_email(self, email):
@@ -68,7 +68,7 @@ class UpdateAboutForm(FlaskForm):
     submit = SubmitField(_l('Confirm'))
 
 class UpdateCategoryForm(FlaskForm):
-    category = StringField(_l('New Category'), validators=[DataRequired()])
+    category = SelectField('Category', choices=[('People', 'People'), ('Restaurant', 'Restaurant'), ('Code Editor', 'Code Editor'), ('School','School')])
     submit = SubmitField(_l('Confirm'))
 
 class UpdateNameForm(FlaskForm):
