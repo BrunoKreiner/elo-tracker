5ad6affe63b856492a7b2f07eb0cdeda8e7778ee
Amr Tagel-Din <aht14@duke.edu>
2021-11-09 15:54:07 -0500

    expanded on events tables and adding

--- app/events_page.py
+++ app/events_page.py
@@ -4,17 +4,57 @@ import datetime
 
 from .models.events import Events
 
+from flask import render_template, redirect, url_for, flash, request
+from werkzeug.urls import url_parse
+from flask_login import login_user, logout_user, current_user
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from flask_babel import _, lazy_gettext as _l
+
 
 from flask import Blueprint
 bp = Blueprint('events_page', __name__)
 
+class EventsForm(FlaskForm):
+    event_id = StringField(_l('event_id'), validators=[DataRequired()])
+    name = StringField(_l('name'), validators=[DataRequired()])
+    type = StringField(_l('type'), validators=[DataRequired()])
+    date = StringField(_l('date'), validators=[DataRequired()])
 
-@bp.route('/events_page')
+    submit = SubmitField(_l('Add Event'))
+
+
+
+@bp.route('/events_page', methods = ['GET', 'POST'])
 def events_page():
-    # get table displaying all leagues:
+    # get tables displaying events in different ways:
     events_table = Events.get_all()
+    events_past_table = Events.get_all_past()
+    events_future_table = Events.get_all_future()
+
+    # create a form to add an event
+    form = EventsForm()
+    if form.validate_on_submit():
+        if Events.addEvent(form.event_id.data,
+        form.name.data,
+        form.type.data,
+        form.date.data):
+            flash('Congratulations, you have added a new Event!')
+            return redirect(url_for('events_page.events_page'))
+
 
     # render the page by adding information to the index.html file
     return render_template('events_page.html',
-                           events_table=events_table)
+                           events_table=events_table,
+                           events_past_table = events_past_table,
+                           events_future_table = events_future_table,
+                           form = form)
 
+
+ButtonPressed = 0        
+@bp.route('/events_page', methods=["GET", "POST"])
+def button():
+    #if request.method == "POST":
+     #   return render_template("events_page.html", ButtonPressed = ButtonPressed)
+    return render_template("events_page.html", ButtonPressed = ButtonPressed)
--- app/models/events.py
+++ app/models/events.py
@@ -1,7 +1,8 @@
 from flask import current_app as app
+from datetime import datetime
 
-class Events:
-    
+
+class Events:    
     def __init__(self, event_id, name, type,date):
         self.event_id = event_id
         self.name= name
@@ -9,6 +10,7 @@ class Events:
         self.date =date
 
 
+# method to get every single event
     @staticmethod
     def get_all():
         rows = app.db.execute('''
@@ -17,9 +19,42 @@ FROM Events
 ''')
         return [Events(*row) for row in rows]
 
-  # define method to add an event     
+  # method to get all past events     
+    @staticmethod
+    def get_all_past():
+        currentDateTime = datetime.now()
 
-    
+        rows = app.db.execute('''
+SELECT event_id, name, type,date
+FROM Events
+WHERE date < (:currentDateTime)
+''', currentDateTime = currentDateTime)
+        return [Events(*row) for row in rows]
+
+ # method to get all past events     
+    @staticmethod
+    def get_all_future():
+        currentDateTime = datetime.now()
+
+        rows = app.db.execute('''
+SELECT event_id, name, type,date
+FROM Events
+WHERE date > (:currentDateTime)
+''', currentDateTime = currentDateTime)
+        return [Events(*row) for row in rows]
+
+
+    # method to add a new league.
+    @staticmethod
+    def addEvent(event_id, name, type, date): 
+        rows = app.db.execute("""
+INSERT INTO Events(event_id, name, type, date)
+VALUES(:event_id, :name, :type, :date)
+RETURNING event_id
+""", event_id=event_id, name=name, type=type, date = date)
+        event_id = rows[0][0] 
+        return event_id
+
 
 
 
