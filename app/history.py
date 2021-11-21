ac94028a5891c2ff470f68f8a271aac097299cc6
Annie Wang <aw364@duke.edu>
2021-11-21 12:45:30 -0500

    pagination works

--- app/history.py
+++ app/history.py
@@ -13,54 +13,66 @@ bp = Blueprint('history', __name__)
 
 @bp.route('/data', methods=['POST', 'GET'])
 def data():
+
+    start_date = None
+    end_date = None
+    filtered_activity = None
+
+    if request.method == "POST":
+        print("YASS")
+        print(request.form)
+
+    if request.method == "POST":
+        if request.form['filterActivity'] != 'None':
+            print('filtered_activity is: ', request.form['filterActivity'])
+            filtered_activity = request.form['filterActivity']
+
+        date_format = "%Y-%m-%d"
+        try:
+            datetime.strptime(request.form['startDate'], date_format)
+            datetime.strptime(request.form['endDate'], date_format)
+            start_date = request.form['startDate']
+            end_date = request.form['endDate']
+            print("This is the correct date string format.")
+        except ValueError:
+            print("This is the incorrect date string format. It should be MM-DD-YYYY")
+
     if not current_user.is_authenticated:
         return redirect(url_for('rankables.login'))
+
     now = datetime.now()
     
     query = Match.get_user_history_full(current_user.rankable_id, None, None, None, now)
+    total_unfiltered = len(query)
+
+    query = Match.get_user_history_full(current_user.rankable_id, filtered_activity, start_date, end_date, now)
     total_filtered = len(query)
+    print('total_filtered is :', total_filtered)
 
-    if request.method == "POST":
-        start = request.form['start']
-        length = request.form['length']
+    # pagination 
+    start = request.form['start']
+    length = request.form['length']
 
-    # pagination
-    start = request.args.get('start')
-    length = request.args.get('length')
-    print('start is', start)
-    #start = request.form['start']
-    #length = request.form['length']
-    query = Match.get_user_history(current_user.rankable_id, None, None, None, now, start, length)
+    # sorting
+    col_index = request.form['order[0][column]']
+    col_name = request.form[f'columns[{col_index}][data]']
+    if col_name == None:
+        col_name = 'date_time'
 
-    # DELETE - TEMP
-    query = Match.get_all()
-    print('len(query) is: ', len(query))
-    total_length = len(query)
-    query = Match.get_all_offset(start, length)
+    asc = 'ASC'
+    if request.form[f'order[0][dir]'] == 'desc':
+        asc = 'DESC'
+
+    query = Match.get_user_history(current_user.rankable_id, filtered_activity, start_date, end_date, now, start, length, col_name, asc)
+    print('queries done')
 
     # response
     return {
         'data': [user.to_dict() for user in query],
-        'recordsFiltered': total_length,
-        'recordsTotal': total_length,
-        'draw': int(request.args.get('draw')),
+        'recordsFiltered': total_filtered,
+        'recordsTotal': total_unfiltered,
+        'draw': int(request.form['draw']),
     }
-
-    print('start is: ', start)
-    print('yeah')
-    #matches = Match.get_all()
-    matches = Match.get_user_history(current_user.rankable_id, None, None, None, now)
-    #myActivities = Match.get_user_activities(current_user.rankable_id, now)
-    #print(myActivities)
-    # find the products current user has bought:
-    #if current_user.is_authenticated:
-    #    purchases = Purchase.get_all_by_uid_since(
-    #        current_user.id, datetime.datetime(1980, 9, 14, 0, 0, 0))
-    #else:
-    #    purchases = None
-    # render the page by adding information to the index.html file
-
-    return {'data': [m.to_dict() for m in matches]}
 
 
 @bp.route('/history', methods=['POST', 'GET'])
@@ -74,26 +86,28 @@ def history():
 
     if request.method == "POST":
         print("/createlist request.method == POST")
-        print(request.form)
+        #print(request.form)
         if request.form['activity'] != 'all':
             print(request.form['activity'])
             filtered_activity = request.form['activity']
 
-        date_format = "%m-%d-%Y"
+        date_format = "%Y-%m-%d"
         try:
             datetime.strptime(request.form['start-date'], date_format)
             datetime.strptime(request.form['end-date'], date_format)
             start_date = request.form['start-date']
+            start_time = start_date
             end_date = request.form['end-date']
+            end_time = end_date
             print("This is the correct date string format.")
         except ValueError:
             print("This is the incorrect date string format. It should be MM-DD-YYYY")
+            print('yeah so the format is currently', request.form['start-date'])
         
 
     # get all available products for sale:
-    matches = Match.get_user_history(current_user.rankable_id, filtered_activity, start_date, end_date, now)
+    #matches = Match.get_user_history(current_user.rankable_id, filtered_activity, start_date, end_date, now)
     myActivities = Match.get_user_activities(current_user.rankable_id, now)
-    print(myActivities)
     # find the products current user has bought:
     #if current_user.is_authenticated:
     #    purchases = Purchase.get_all_by_uid_since(
@@ -101,7 +115,7 @@ def history():
     #else:
     #    purchases = None
     # render the page by adding information to the index.html file
-    return render_template('history.html', my_activities=myActivities) #,
-                           #avail_products=products,
-                           #purchase_history=purchases)
+    print('filtered activity from history is: ', filtered_activity)
+    return render_template('history.html', my_activities=myActivities, filterActivity=filtered_activity,
+                                            start_date=start_date, end_date=end_date) 
 
--- app/models/match.py
+++ app/models/match.py
@@ -5,19 +5,24 @@ def sortingFunc(match):
         return match[1]
 
 class Match:
-    def __init__(self, activity, id, user1_id, user2_id, user1_score, user2_score, datetime):
+    def __init__(self, activity, id, user1_id, user2_id, user1_score, user2_score, datetime, user2_name=None):
         self.activity = activity
         self.id = id
         self.user1_id = user1_id
         self.user2_id = user2_id
+        self.user2_name = user2_name
         self.user1_score = user1_score
         self.user2_score = user2_score
         self.datetime = datetime
 
     def to_dict(self):
+
         return {
             'activity': self.activity,
-            'match ID': self.id
+            'user2_name': self.user2_name,
+            'user1_score': self.user1_score,
+            'user2_score': self.user2_score,
+            'date_time': self.datetime
         }
 
     @staticmethod
@@ -66,7 +71,6 @@ WHERE user1_ID = :user_id AND date_time < :curr_datetime
         rows = []
 
         if activity is None and start_time is None:
-            print('yes')
             matches_user1 = app.db.execute('''
     SELECT activity, matchID, user1_ID, user2_ID, user1_score, user2_score, date_time
     FROM Matches
@@ -81,7 +85,6 @@ WHERE user1_ID = :user_id AND date_time < :curr_datetime
                                 user_id=user_id, curr_datetime=curr_datetime)
             rows += matches_user1 + matches_user2
         elif start_time is None:
-            print('yes')
             matches_user1 = app.db.execute('''
     SELECT activity, matchID, user1_ID, user2_ID, user1_score, user2_score, date_time
     FROM Matches
@@ -131,85 +134,111 @@ WHERE user1_ID = :user_id AND date_time < :curr_datetime
                                 user_id=user_id, curr_datetime=curr_datetime, start_time=start_time, end_time=end_time, activity=activity)
             rows += matches_user1 + matches_user2
         #rows = matches_user1 + matches_user2
-        print(rows)
         rows.sort(key=sortingFunc)
         rows = [Match(*row) for row in rows]
         return rows
 
     @staticmethod
-    def get_user_history(user_id, activity, start_time, end_time, curr_datetime, start_page=0, offset_lim=3):
+    def get_user_history(user_id, activity, start_time, end_time, curr_datetime, start_page, offset_lim, sortBy, asc):
         rows = []
 
         if activity is None and start_time is None:
-            print('yes')
-            matches_user1 = app.db.execute('''
-    SELECT activity, matchID, user1_ID, user2_ID, user1_score, user2_score, date_time
+
+            rows = app.db.execute('''
+
+    SELECT T.activity, T.matchID, T.user1_ID, T.user2_ID, T.user1_score, T.user2_score, T.date_time, Rankables.name
+    FROM
+    
+    ((SELECT activity, matchID, user1_ID, user2_ID, user1_score, user2_score, date_time
     FROM Matches
-    WHERE user1_ID = :user_id AND date_time < :curr_datetime
+    WHERE user1_ID = :user_id AND date_time < :curr_datetime)
+    UNION
+    (SELECT activity, matchID, user2_ID AS user1_ID, user1_ID AS user2_ID, user2_score, user1_score, date_time
+    FROM Matches
+    WHERE user2_ID = :user_id AND date_time < :curr_datetime)) AS T,
+    Rankables
+    WHERE Rankables.rankable_id = T.user2_ID
+
+    ORDER BY {} {}
     LIMIT :limit
     OFFSET :start_page
-    ''',
-                                user_id=user_id, curr_datetime=curr_datetime, limit=offset_lim, start_page=start_page)
-            matches_user2 = app.db.execute('''
-    SELECT activity, matchID, user2_ID, user1_ID, user2_score, user1_score, date_time
-    FROM Matches
-    WHERE user2_ID = :user_id AND date_time < :curr_datetime
-    ''',
-                                user_id=user_id, curr_datetime=curr_datetime)
-            rows += matches_user1 + matches_user2
+    '''.format(sortBy, asc),
+            user_id=user_id, curr_datetime=curr_datetime, limit=offset_lim, start_page=start_page)
+
         elif start_time is None:
-            print('yes')
-            matches_user1 = app.db.execute('''
-    SELECT activity, matchID, user1_ID, user2_ID, user1_score, user2_score, date_time
+
+            rows = app.db.execute('''
+
+    SELECT T.activity, T.matchID, T.user1_ID, T.user2_ID, T.user1_score, T.user2_score, T.date_time, Rankables.name
+    FROM
+    
+    ((SELECT activity, matchID, user1_ID, user2_ID, user1_score, user2_score, date_time
     FROM Matches
-    WHERE user1_ID = :user_id AND date_time < :curr_datetime AND activity = :activity
-    ''',
-                                user_id=user_id, curr_datetime=curr_datetime, activity=activity)
-            matches_user2 = app.db.execute('''
-    SELECT activity, matchID, user2_ID, user1_ID, user2_score, user1_score, date_time
+    WHERE user1_ID = :user_id AND date_time < :curr_datetime AND activity = :activity)
+    UNION
+    (SELECT activity, matchID, user2_ID AS user1_ID, user1_ID AS user2_ID, user2_score, user1_score, date_time
     FROM Matches
-    WHERE user2_ID = :user_id AND date_time < :curr_datetime AND activity = :activity
-    ''',
-                                user_id=user_id, curr_datetime=curr_datetime, activity=activity)
-            rows += matches_user1 + matches_user2
+    WHERE user2_ID = :user_id AND date_time < :curr_datetime AND activity = :activity)) AS T,
+    Rankables
+    WHERE Rankables.rankable_id = T.user2_ID
+
+    ORDER BY {} {}
+    LIMIT :limit
+    OFFSET :start_page
+    '''.format(sortBy, asc),
+            user_id=user_id, curr_datetime=curr_datetime, activity=activity, limit=offset_lim, start_page=start_page)
 
         elif activity is None:
-            matches_user1 = app.db.execute('''
-    SELECT activity, matchID, user1_ID, user2_ID, user1_score, user2_score, date_time
+
+            rows = app.db.execute('''
+
+    SELECT T.activity, T.matchID, T.user1_ID, T.user2_ID, T.user1_score, T.user2_score, T.date_time, Rankables.name
+    FROM
+    
+    ((SELECT activity, matchID, user1_ID, user2_ID, user1_score, user2_score, date_time
     FROM Matches
     WHERE user1_ID = :user_id AND date_time < :curr_datetime
-    AND date_time > :start_time AND date_time < :end_time
-    ''',
-                                user_id=user_id, curr_datetime=curr_datetime, start_time=start_time, end_time=end_time)
-            matches_user2 = app.db.execute('''
-    SELECT activity, matchID, user2_ID, user1_ID, user2_score, user1_score, date_time
+    AND date_time > :start_time AND date_time < :end_time)
+    UNION
+    (SELECT activity, matchID, user2_ID AS user1_ID, user1_ID AS user2_ID, user2_score, user1_score, date_time
     FROM Matches
     WHERE user2_ID = :user_id AND date_time < :curr_datetime 
-    AND date_time > :start_time AND date_time < :end_time
-    ''',
-                                user_id=user_id, curr_datetime=curr_datetime, start_time=start_time, end_time=end_time)
-            rows += matches_user1 + matches_user2
+    AND date_time > :start_time AND date_time < :end_time)) AS T,
+    Rankables
+    WHERE Rankables.rankable_id = T.user2_ID
+
+    ORDER BY {} {}
+    LIMIT :limit
+    OFFSET :start_page
+    '''.format(sortBy, asc),
+            user_id=user_id, curr_datetime=curr_datetime, start_time=start_time, end_time=end_time, limit=offset_lim, start_page=start_page)
+
         else:
-            matches_user1 = app.db.execute('''
-    SELECT activity, matchID, user1_ID, user2_ID, user1_score, user2_score, date_time
+            rows = app.db.execute('''
+
+    SELECT T.activity, T.matchID, T.user1_ID, T.user2_ID, T.user1_score, T.user2_score, T.date_time, Rankables.name
+    FROM
+    
+    ((SELECT activity, matchID, user1_ID, user2_ID, user1_score, user2_score, date_time
     FROM Matches
     WHERE user1_ID = :user_id AND date_time < :curr_datetime
     AND date_time > :start_time AND date_time < :end_time
-    AND activity = :activity
-    ''',
-                                user_id=user_id, curr_datetime=curr_datetime, start_time=start_time, end_time=end_time, activity=activity)
-            matches_user2 = app.db.execute('''
-    SELECT activity, matchID, user2_ID, user1_ID, user2_score, user1_score, date_time
+    AND activity = :activity)
+    UNION
+    (SELECT activity, matchID, user2_ID AS user1_ID, user1_ID AS user2_ID, user2_score, user1_score, date_time
     FROM Matches
     WHERE user2_ID = :user_id AND date_time < :curr_datetime 
     AND date_time > :start_time AND date_time < :end_time
-    AND activity = :activity
-    ''',
-                                user_id=user_id, curr_datetime=curr_datetime, start_time=start_time, end_time=end_time, activity=activity)
-            rows += matches_user1 + matches_user2
-        #rows = matches_user1 + matches_user2
-        print(rows)
-        rows.sort(key=sortingFunc)
+    AND activity = :activity)) AS T,
+    Rankables
+    WHERE Rankables.rankable_id = T.user2_ID
+
+    ORDER BY {} {}
+    LIMIT :limit
+    OFFSET :start_page
+    '''.format(sortBy, asc),
+            user_id=user_id, curr_datetime=curr_datetime, start_time=start_time, end_time=end_time, activity=activity, limit=offset_lim, start_page=start_page)
+
         rows = [Match(*row) for row in rows]
         return rows
 
